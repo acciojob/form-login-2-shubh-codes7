4d42b00bbbe774f3b0cedfef35d6ad0d08c8214e
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-16 19:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+//your JS code here. If required
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+	let fname = document.querySelector("#fname").value;
+	let lname = document.querySelector("#lname").value;
+	let phone = document.querySelector("#tel").value;
+	let email = document.querySelector("#email").value;
+	alert(`First Name: ${fname} Last Name: ${lname} Phone Number: ${phone} Email ID: ${email}`)
+})
+
